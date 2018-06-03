7b55e5b59c33565b3beda42e3cf506da72a72ed7
Vasanth <jm_vasanth@hotmail.com>
2018-06-03 20:37:04 +1000

    Added new python example files

---
+++ ex2.py
@@ -0,0 +1,3 @@
+print("This line will be printed") # This is the comment
+#print("This line will be printed and comment should be printed")#This is the comment
+print("This will run.")
---
+++ ex3.py
@@ -0,0 +1,4 @@
+print("Hens", 25 + 30 / 6)
+print("Roosters", 75 % 4)
+print("Divide", 5 % 4)
+print("Is it greater or equal?", 5 <= -2)
